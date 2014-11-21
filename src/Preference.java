5d2c50f1c86b853addf52b60c031de503a489a63
Tony Huang <jianhuanga@gmail.com>
2014-11-21 15:00:39 +0800

    update.

---
+++ src/Preference.java
@@ -0,0 +1,12 @@
+
+public class Preference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   int x = 1, y = 2, z = 3;
+		   int a = x + y - 2 /2 + z;
+		   int b = x + (y - 2)/(2 + z);
+		   System.out.println("a =  " + a + " b = " + b);
+	}
+
+}
