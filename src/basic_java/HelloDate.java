fdfc3839418504b56fbfabd98e0e9bb897b8997e
Tony Huang <jianhuanga@gmail.com>
2014-11-21 10:12:15 +0800

    init java sloth.

---
+++ src/basic_java/HelloDate.java
@@ -0,0 +1,18 @@
+package basic_java;
+import  java.util.*;
+
+/** The first Thinking in Java example program.
+ * Displays a string and today's date.
+ * @author Bruce Eckel
+ * @author www.MindView.net
+ * @version 4.0
+*/
+public class HelloDate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     System.out.print("Hello, it's: ");
+	     System.out.println(new Date());
+	}
+
+}
