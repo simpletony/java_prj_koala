f59aefe3a65b5406e075739e0038dbf36d46ad43
Tony Huang <jianhuanga@gmail.com>
2014-11-24 17:26:51 +0800

    update.

---
+++ src/AutoInc.java
@@ -0,0 +1,17 @@
+import static net.mindview.util.Print.*;
+
+public class AutoInc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		print("i :" + i);
+		print("++ i :" + ++i);
+		print("i ++ :" + i++);
+		print("i :" + i);
+		print("--i :" + --i);
+		print("i-- :" + i--);
+		print("i :" + i);
+	}
+
+}
