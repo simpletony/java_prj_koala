5514face50325ce34f9e5c36fd23aaba04ba442e
Tony Huang <jianhuanga@gmail.com>
2014-11-26 10:43:13 +0800

    update.

---
+++ src/BitManipulation.java
@@ -0,0 +1,17 @@
+import  java.util.*;
+import static net.mindview.util.Print.*;
+
+public class BitManipulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random rand = new Random(47);
+		int i = rand.nextInt();
+		int j = rand.nextInt();
+		
+//		printBinaryInt
+//		printBinaryInt("-1", -1);
+		
+	}
+
+}
